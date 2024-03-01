8177d51c43161bc71f07f0e5e6068e3e5a133e39
Augustine Ugwu <105177548+augustine-ugwu@users.noreply.github.com>
2024-03-01 13:47:14 +0100

    MOD Project

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const express = require("express");
-const connectDB = require('./db')
-const Post = require('./models/postModel')
-const User = require('./models/userModel')
-const bcrypt = require('bcryptjs')
-
+const connectDB = require("./db");
+const Post = require("./models/postModel");
+const User = require("./models/userModel");
+const bcrypt = require("bcryptjs");
 
 //Init app & middleware
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // connect db
-connectDB()
+connectDB;
 
 //routes
-app.get("/books", (req, res) => {
+app.get("/M00877074", (req, res) => {
   res.json({ msg: "welcome to the api" });
 });
 
+app.post("/M00877074/register", async (req, res) => {
+  const { username, profile, password, profile_info } = req.body;
 
+  // encrypt password
+  const salt = bcrypt.genSalt(10);
+  const hashed_password = bcrypt.hash(password, salt);
+  const post = await User.create({
+    username,
+    email,
+    profile,
+    password: hashed_password,
+  });
 
+  if (post) {
+    res.json(post);
+  }
+});
 
-app.listen(4000, ()=>{
-  console.log("App is listening on port 4000")
-})
+app.listen(4000, () => {
+  console.log("App is listening on port 4000");
+});
--- db.js
+++ db.js
@@ -1,35 +1,35 @@
-// const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
-// let dbconnection 
+let dbconnection;
 
-// module.exports = {
-//     connectTodb: (cb) => {
-//         MongoClient.connect('mongodb://localhost:27017/bookstore')
-//         .then((client) => {
-//             dbconnection = client.db()
-//             return cb()
-//         })
-//         .catch(err => {
-//             console.log(err)
-//             return cb(err)
-//         })
-//     },
-//     getDb: () => dbConnection
-// }
+module.exports = {
+  connectTodb: (cb) => {
+    MongoClient.connect("mongodb://localhost:27017/bookstore")
+      .then((client) => {
+        dbconnection = client.db();
+        console.log(`MongoDB connected: ${dbconnection}`);
+        return cb();
+      })
+      .catch((err) => {
+        console.log(err);
+        return cb(err);
+      });
+  },
+  getDb: () => dbConnection,
+};
 
-const mongoose = require('mongoose')
+// const mongoose = require("mongoose");
 
 // const URL = 'mongodb+srv://ukejemicheal:HFshR6WpLVRrtSls@cluster0.csedbmx.mongodb.net'
-const URL = 'mongodb://localhost:27017/parent_connect'
-const connectDB = async ()=>{
-    try{
-        const connect = await mongoose.connect(URL)
-        console.log(`MongoDB connected: ${connect.connection.host}`)
-    }
-    catch(error){
-        console.log(`error: ${error}`)
-        process.exit(1)
-    }
-}
+// const URL = "mongodb://localhost:27017/parent_connect";
+// const connectDB = async () => {
+//   try {
+//     const connect = await mongoose.connect(URL);
+//     console.log(`MongoDB connected: ${connect.connection.host}`);
+//   } catch (error) {
+//     console.log(`error: ${error}`);
+//     process.exit(1);
+//   }
+// };
 
-module.exports = connectDB
+// module.exports = connectDB;
