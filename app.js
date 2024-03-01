f55bb618ab4fece2bec7acb4b9f15ab990684d8a
ndmichael <ukejemicheal@gmail.com>
2024-03-01 14:53:08 +0100

    connect to mongodb

--- app.js
+++ app.js
@@ -15,10 +15,60 @@ app.use(express.urlencoded({extended: false}))
 connectDB()
 
 //routes
-app.get("/books", (req, res) => {
-  res.json({ msg: "welcome to the api" });
-});
+app.get('/M00877074/posts', async (req, res)=>{
+  const posts = await Post.find()
+  res.status(200).json(posts)
+})
 
+// register user
+app.post('/M00877074/user/register', async (req, res)=>{
+  const {username, email, profile_info, password} = req.body
+  
+  if(!username || !email || !profile_info || !password){
+      res.status(400)
+      throw new Error(`Please body can't be empty`)
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashed_password = await bcrypt.hash(password, salt)
+
+  // create user
+  const user = await User.create({
+      username,
+      profile_info,
+      email,
+      password: hashed_password
+  })
+
+    res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email
+    })
+})
+
+
+// user post content
+app.post('/M00877074', async (req, res)=>{
+  const {author, content} = req.body
+  
+  if(!author || !content){
+      res.status(400)
+      throw new Error(`Please body can't be empty`)
+  }
+
+  // create user
+  const post = await Post.create({
+      author,
+      content
+  })
+
+    res.status(201).json({
+        _id: post.id,
+        content : post.content
+    })
+})
 
 
 
--- db.js
+++ db.js
@@ -1,26 +1,7 @@
-// const { MongoClient } = require('mongodb')
-
-// let dbconnection 
-
-// module.exports = {
-//     connectTodb: (cb) => {
-//         MongoClient.connect('mongodb://localhost:27017/bookstore')
-//         .then((client) => {
-//             dbconnection = client.db()
-//             return cb()
-//         })
-//         .catch(err => {
-//             console.log(err)
-//             return cb(err)
-//         })
-//     },
-//     getDb: () => dbConnection
-// }
-
 const mongoose = require('mongoose')
 
-// const URL = 'mongodb+srv://ukejemicheal:HFshR6WpLVRrtSls@cluster0.csedbmx.mongodb.net'
-const URL = 'mongodb://localhost:27017/parent_connect'
+const URL = ''
+// const URL = 'mongodb://localhost:27017/parent_connect'
 const connectDB = async ()=>{
     try{
         const connect = await mongoose.connect(URL)
