527b27ebda0a95858a0d079de9d9647d446225ed
ndmichael <ukejemicheal@gmail.com>
2024-03-01 12:37:22 +0100

    changes made

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require("express");
-const { connecToDb, getDb, connectTodb } = require("./db");
+const connectDB = require('./db')
+const Post = require('./models/postModel')
+const User = require('./models/userModel')
+const bcrypt = require('bcryptjs')
+
 
 //Init app & middleware
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-//db connection
-let db;
 
-connectTodb((err) => {
-  if (!err) {
-    app.listen(3000, () => {
-      console.log("app listening on port 3000");
-    });
-    db = getDb();
-  }
-});
+// connect db
+connectDB()
 
 //routes
 app.get("/books", (req, res) => {
   res.json({ msg: "welcome to the api" });
 });
+
+
+
+
+app.listen(4000, ()=>{
+  console.log("App is listening on port 4000")
+})
